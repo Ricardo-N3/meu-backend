704f47a47edd713525c705fad95ed3dd3d8bb7f0
Ricardo-N3 <Nepomucenoricardo55@gmail.com>
2025-08-10 11:48:48 -0300

    Adiciona rota inicial

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth//login-form")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login-form")
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"mensagem": "API FastAPI rodando no Render!"}
 
 from auth_routes import auth_router
 from order_routes import order_router
